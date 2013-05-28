e1f54560eeeab83a35e4328574db306182f6c7e2
407456 <407456@inf0823.ICEI.local>
2013-05-28 10:48:34 -0300

    Commit de consistência

--- src/ocr/entities/OCR.java
+++ src/ocr/entities/OCR.java
@@ -4,26 +4,140 @@
  */
 package ocr.entities;
 
+import java.util.List;
+import java.util.Map;
 import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.learning.DataSet;
+import org.neuroph.core.learning.DataSetRow;
+import org.neuroph.imgrec.image.Image;
 
 /**
  * Entidade da classe OCR
+ *
  * @author Bruno, Mívian e Washington
  */
 public class OCR {
-    NeuralNetwork rede;
-    
+
+    private NeuralNetwork rede;
+    private Map<Image, Integer> treino;
+    private DataSet conjunto;
+
     //CONSTRUTORES
     public OCR() {
     }//end construtor
-        
-    public OCR(NeuralNetwork rede) {
+
+    public OCR(NeuralNetwork rede, Map<Image, Integer> treino) {
         this.rede = rede;
+        this.treino = treino;
     }//end construtor
 
     //MÉTODOS
-    
-    
+    /**
+     * Treina a rede neural
+     */
+    public void treinarRede() {
+        rede.learn(this.conjunto);
+    }//end treinarRede
+
+    /**
+     * Adiciona uma imagem ao conjunto de treino
+     *
+     * @param imagem
+     * @param classe
+     */
+    public void adicionarImagemAoTreino(Image imagem, Integer classe) {
+        int[] pixels;
+        double[] entrada;
+        double[] saidaEsperada;
+
+        this.treino.put(imagem, classe);//Adiciona a imagem no vetor de treino
+
+        //Obtém o vetor de pixels da imagem
+        pixels = imagem.getPixels(
+                0,//Offset
+                1,//Stride
+                0,//x
+                0,//y
+                imagem.getWidth(),//Largura
+                imagem.getHeight()
+                );//Altura
+
+        //Realizando conversão para Double
+        entrada = new double[pixels.length];
+        saidaEsperada = new double[1];
+
+        for (int i = 0; i < pixels.length; i++) {
+            entrada[i] = (double) pixels[i];
+        }//end for        
+        saidaEsperada[0] = (double) classe;
+
+        //Adicionando no conjunto de treinamento
+        this.conjunto.addRow(entrada, saidaEsperada);
+    }//end adicionarImagemAoTreino
+
+    /**
+     * Adiciona uma imagem rotulada ao conjunto de treino
+     * @param imagem
+     * @param classe
+     */
+    public void adicionarImagemAoTreino(Image imagem, Integer classe,
+            String rotulo) {
+        int[] pixels;
+        double[] entrada;
+        double[] saidaEsperada;
+        DataSetRow dados;
+
+        this.treino.put(imagem, classe);//Adiciona a imagem no vetor de treino
+
+        //Obtém o vetor de pixels da imagem
+        pixels = imagem.getPixels(
+                0,//Offset
+                1,//Stride
+                0,//x
+                0,//y
+                imagem.getWidth(),//Largura
+                imagem.getHeight()
+                );//Altura
+
+        //Realizando conversão para Double
+        entrada = new double[pixels.length];
+        saidaEsperada = new double[1];
+
+        for (int i = 0; i < pixels.length; i++) {
+            entrada[i] = (double) pixels[i];
+        }//end for        
+        saidaEsperada[0] = (double) classe;
+        
+        //Define os dados (pixels) e o rótulo
+        dados = new DataSetRow(entrada, saidaEsperada);
+        dados.setLabel(rotulo);
+        
+        //Adicionando no conjunto de treinamento
+        this.conjunto.addRow(dados);
+    }//end adicionarImagemAoTreino
+
+    /**
+     * Retorna a classe da imagem removida
+     * @param imagem
+     * @return classe
+     */
+    public Integer removerImagemDoTreino(Image imagem, String rotulo) {
+        int indice = -1;
+        List<DataSetRow> imagens = this.conjunto.getRows();
+
+        for (int i = 0; i < imagens.size(); i++) {
+            if (imagens.get(i).getLabel().equalsIgnoreCase(rotulo)) {
+                indice = i;
+                i = imagens.size();
+            }//end if
+        }//end for
+
+        //Remove imagem do conjunto de treino
+        this.conjunto.removeRowAt(indice);
+
+        return this.treino.remove(imagem);
+    }//end removerImagemDoTreino
+
     //GETTERS AND SETTERS
     public NeuralNetwork getRede() {
         return rede;
@@ -32,5 +146,12 @@ public class OCR {
     public void setRede(NeuralNetwork rede) {
         this.rede = rede;
     }
-           
-}
+
+    public Map<Image, Integer> getTreino() {
+        return treino;
+    }
+
+    public void setTreino(Map<Image, Integer> treino) {
+        this.treino = treino;
+    }
+}//end class
